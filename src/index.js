fb5a326a30f2da07532030d72e148ef6e57f8b2c
Nataliya R <n.rudenko@gmail.com>
2020-01-18 23:46:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import catsReducer from './reducers/catsReducer.js';
 
 const store = createStore(catsReducer)
