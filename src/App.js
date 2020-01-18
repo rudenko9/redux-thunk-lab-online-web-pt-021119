f884e0843b2075af64c79a4dac09061b196323ba
Nataliya R <n.rudenko@gmail.com>
2020-01-18 23:54:07 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class App extends Component {   
-  
+class App extends Component {
+
   render() {
     return (
       <div>
@@ -14,4 +14,3 @@ class App extends Component {
 }
 
 export default App
-
